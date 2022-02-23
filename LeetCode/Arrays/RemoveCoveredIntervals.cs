1d2eb114532bcf66d9bf86d68428f31d3a68fc15
ShaharShokrani <45237118+ShaharShokrani@users.noreply.github.com>
2022-02-23 21:24:23 +0200

    Arrays
    Arrays

---
+++ LeetCode/Arrays/RemoveCoveredIntervals.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/problems/remove-covered-intervals/
+    /// </summary>
+    [TestClass]
+    public class RemoveCoveredIntervalsClass
+    {
+        [TestMethod]
+        public void TwoSumTest()
+        {
+            Assert.AreEqual(1, RemoveCoveredIntervals(new int[][]
+                                        {
+                                        new int[] { 1, 2 },
+                                        new int[] { 1, 4 },
+                                        new int[] { 3, 4 }
+                                        }));
+
+            Assert.AreEqual(2, RemoveCoveredIntervals(new int[][]
+                                        {
+                                        new int[] { 3, 10 },
+                                        new int[] { 4, 10 },
+                                        new int[] { 5, 11 }
+                                        }));
+            Assert.AreEqual(2, RemoveCoveredIntervals(new int[][]
+                                        {
+                                        new int[] { 1, 4 },
+                                        new int[] { 3, 6 },
+                                        new int[] { 2, 8 }
+                                        }));
+            Assert.AreEqual(1, RemoveCoveredIntervals(new int[][]
+                                        {
+                                        new int[] { 1, 4 },
+                                        new int[] { 2, 3 }
+                                        }));
+        }
+
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            int result = 0;
+
+            IOrderedEnumerable<int[]> orderedIntervals = intervals.OrderBy(x => x.First()).ThenByDescending(x => x.ElementAt(1));
+
+            int previousA = -1;
+            int previousB = -1;
+            
+            foreach (int[] orderedInterval in orderedIntervals)
+            {
+                int currentA = orderedInterval[0];
+                int currentB = orderedInterval[1];
+
+                if (currentA >= previousA && currentB <= previousB)
+                {
+                    result++;
+                }
+                else
+                {
+                    previousA = currentA;
+                    previousB = currentB;
+                }
+            }
+
+            return intervals.Length - result;
+        }
+    }
+}
---
+++ LeetCode/Arrays/TwoSum.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Arrays.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    [TestClass]
+    public class TwoSumClass
+    {
+        [TestMethod, Timeout(2000)]
+        public void TwoSumTest()
+        {
+            CollectionAssert.AreEquivalent(new int[] { 0, 1 }, TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+            CollectionAssert.AreEquivalent(new int[] { 1, 2 }, TwoSum(new int[] { 3, 2, 4 }, 6));
+            CollectionAssert.AreEquivalent(new int[] { 0, 1 }, TwoSum(new int[] { 3, 3 }, 6));
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            IDictionary<int, int> keyValues = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int lookup = target - num;
+                    
+                if (keyValues.ContainsKey(lookup))
+                {
+                    result[0] = keyValues[lookup];
+                    result[1] = i;
+
+                    return result;
+                }
+
+                if (!keyValues.ContainsKey(num))
+                {
+                    keyValues[num] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
