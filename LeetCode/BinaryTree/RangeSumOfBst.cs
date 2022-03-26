7ee4c7d373888f12c39e24b0850069c7856f0a2d
ShaharShokrani <45237118+ShaharShokrani@users.noreply.github.com>
2022-03-26 23:14:29 +0300

    RangeSumOfBst

---
+++ LeetCode/BinaryTree/RangeSumOfBst.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace LeetCode.BinaryTree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-of-bst/
+    /// </summary>
+    [TestClass]
+    public class RangeSumOfBst
+    {
+        [TestMethod]
+        public void RangeSumBSTTest()
+        {
+            TreeNode tree = new TreeNode(10)
+            {                
+                left = new TreeNode(5)
+                {                    
+                    left = new TreeNode(3),
+                    right = new TreeNode(7)
+                },
+                right = new TreeNode(15)
+                {
+                    right = new TreeNode(18)
+                }
+            };
+            Assert.AreEqual(32, RangeSumBST(tree, 7, 15));
+        }
+
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.val >= low && root.val <= high)
+                return root.val + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+            else
+                return RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+    }
+}
