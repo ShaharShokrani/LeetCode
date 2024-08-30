c9d0f30fa443fd75d7b4d5b3ffc5f397302bce58
ShaharShokrani <109962929+ShaharShokraniOctopai@users.noreply.github.com>
2024-08-30 20:17:42 +0300

    Update FindIfPathExistsInGraph.cs

--- LeetCode/Graphs/FindIfPathExistsInGraph.cs
+++ LeetCode/Graphs/FindIfPathExistsInGraph.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LeetCode.Graphs
 {
@@ -18,7 +20,11 @@ namespace LeetCode.Graphs
                                         new int[] { 1, 2 },
                                         new int[] { 2, 0 }
                             }, 0, 2));
+        }
 
+        [TestMethod, Timeout(2000)]
+        public void FibTest2()
+        {
             Assert.AreEqual(false, ValidPath(6, new int[][]
                             {
                                         new int[] { 0, 1 },
@@ -27,10 +33,118 @@ namespace LeetCode.Graphs
                                         new int[] { 5, 4 },
                                         new int[] { 4, 3 }
                             }, 0, 5));
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void FibTest3()
+        {
+            Assert.AreEqual(true, ValidPath(2, new int[][]
+                            {
+                                        new int[] { 0, 1 },
+                                        new int[] { 1, 2 },
+                            }, 0, 2));
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void FibTest4()
+        {
+            Assert.AreEqual(true, ValidPath(1, new int[][] { }, 0, 0));
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void FibTest5()
+        {
+            Assert.AreEqual(true, ValidPath(10, new int[][] 
+            {
+                new int[] { 4, 3 },
+                new int[] { 1, 4 },
+                new int[] { 4, 8 },
+                new int[] { 1, 7 },
+                new int[] { 6, 4 },
+                new int[] { 4, 2 },
+                new int[] { 7, 4 },
+                new int[] { 4, 0 },
+                new int[] { 0, 9 },
+                new int[] { 5, 4 },
+            }, 5, 9));
+        }
+
+        [TestMethod, Timeout(2000)]
+        public void FibTest6()
+        {
+            Assert.AreEqual(true, ValidPath(10, new int[][]
+            {
+                new int[] { 0, 7 },
+                new int[] { 0, 8 },
+                new int[] { 6, 1 },
+                new int[] { 2, 0 },
+                new int[] { 0, 4 },
+                new int[] { 5, 8 },
+                new int[] { 4, 7 },
+                new int[] { 1, 3 },
+                new int[] { 3, 5 },
+                new int[] { 6, 5 },
+            }, 7, 5));
         }        
+
+        IDictionary<int, List<int>> _edgesList = new Dictionary<int, List<int>>();
+        HashSet<int> _visits = new HashSet<int>();
+
         public bool ValidPath(int n, int[][] edges, int source, int destination)
-        {           
-            throw new NotImplementedException();
+        {
+            if (edges.Length == 0)
+                return true;
+
+            foreach (int[] edge in edges)
+            {
+                int key = edge[0];
+                if (!_edgesList.ContainsKey(key))
+                {
+                    _edgesList.Add(key, new List<int>() { edge[1] });
+                }
+                else
+                {
+                    _edgesList[key].Add(edge[1]);
+                }
+
+                key = edge[1];
+                if (!_edgesList.ContainsKey(key))
+                {
+                    _edgesList.Add(key, new List<int>() { edge[0] });
+                }
+                else
+                {
+                    _edgesList[key].Add(edge[0]);
+                }
+            }
+
+            if (Recursive(source, destination))
+                return true;
+
+            return false;
+        }
+
+        private bool Recursive(int source, int destination)
+        {
+            if (source == destination)
+                return true;
+
+            if (_edgesList.ContainsKey(source))
+            {
+                foreach (int target in _edgesList[source])
+                {
+                    if (_visits.Contains(target))
+                    {
+                        continue;
+                    }
+                    _visits.Add(target);
+
+                    if (Recursive(target, destination))
+                        return true;
+                }
+            }
+
+            return false;            
         }
     }
 }
